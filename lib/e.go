3b5adb78ebe262cdaa7b62e864fae4a4ebf71f12
le_wei <le.wei@derbysoft.net>
2020-05-11 11:14:44 +0800

    依赖冲突

---
+++ lib/e.go
@@ -0,0 +1,11 @@
+package lib
+
+import "fmt"
+
+import "github.com/valvrave-develop/study/d"
+
+
+func Print() {
+	fmt.Println("hello world")
+	d.Print()
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "study_lib/lib"
+
+func main() {
+	lib.Print()
+}
